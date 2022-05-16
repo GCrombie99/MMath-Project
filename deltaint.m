348187550d8a6923424c4f0a78e2a6c0ea77ff2f
GCrombie99 <103449207+GCrombie99@users.noreply.github.com>
2022-05-16 18:34:33 +0100

    Update deltaint.m

--- deltaint.m
+++ deltaint.m
@@ -1,7 +1,7 @@
 function dpdt = deltaint(t,p)
-R = 5.14;
-alpha=0.19;
-delta=1.62;
+R = 1.7;
+alpha=0.2;
+delta=1.6;
 
 dpdt = [p(1) .* (-1 + R*p(2));
         p(2) .* (- R*p(1)-alpha-delta)+alpha];
