daf816b892cb697bdd81c51a71c9ff42018ccaa9
GCrombie99 <103449207+GCrombie99@users.noreply.github.com>
2022-05-16 18:38:03 +0100

    Update Proposition2part1b.m

--- Proposition2part1b.m
+++ Proposition2part1b.m
@@ -1,8 +1,8 @@
-tspan = [10 0];
+tspan = [100 0];
 tstart = tspan(1);
-p0 = [0.156 0.19455];
-Iint=0.156;
-Inat=0.15;
+Iint=0.1;
+Inat=0.05;
+p0 = [Iint 1/1.7];
 t = tstart;
 p = p0;
 [t,p,te,pe,ie]=ode45(@deltanat,[t(end),tspan(end)],p(end, :),odeset('Events',@TerminateNat2));
@@ -11,19 +11,18 @@ hold on
 tspan = [0 100];
 tstart = tspan(1);
 tend = tspan(end);
-p0 = [Iint 0.19455];
+p0 = [Iint 1/1.7];
 t = tstart;
 p = p0;
 [t,p]=ode45(@deltaint,[t(end),tend],p(end,:));
 plot(p(:,1),p(:,2))
 xline(Iint,'--')
 xline(Inat,'--')
-yline(0.19455,'--',{'S_{E}'})
-plot(0.15304, 1/5.14, '.','MarkerSize',15) %Plot E
-plot(0, 0.19/1.81, '.','MarkerSize',15) %Plot E*
+yline(1/1.7,'--')
+plot(0.2*(1-1/1.7), 1/1.7, '.','MarkerSize',15) %Plot E
+plot(0, 0.2/1.8, '.','MarkerSize',15) %Plot E*
 xlim([0 Iint+0.01])
 ylim([0 1.01])
 xlabel('I')
 ylabel('S')
 hold off
-
